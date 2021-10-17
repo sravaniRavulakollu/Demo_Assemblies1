c4d10e2b0c9e23519640e83d0744365df8dae464
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-17 22:23:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Demo_Assemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imlementing Assemblies in C#!!");
+
+            Stringer myStringInstance = new Stringer();
+            Console.WriteLine("Client code executes");
+            myStringInstance.StringerMethod();
+        }
+    }
+}
---
+++ Stringer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo_Assemblies
+{
+    public class Stringer
+    {
+        public void StringerMethod()
+        {
+            System.Console.WriteLine("This is a line from StringerMethod.");
+        }
+    }
+}
